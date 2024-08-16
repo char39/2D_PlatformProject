5642fffb774a905531914db355d689e35ca18cd1
Your Name <seongsun8330@naver.com>
2024-08-16 22:47:45 +0900

    0816_이미지 meta데이터 일괄 변경

--- Assets/Editor/ChangeSpritesSettings.cs
+++ Assets/Editor/ChangeSpritesSettings.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
 using UnityEditor;
 
-// Copilot을 사용하여 작성된 코드입니다.
-
 public class ChangeSpritesSettings : EditorWindow
 {
-    // 상단 Window의 Tools에서 Change Sprites Settings를 추가
     [MenuItem("Tools/Change Sprites Settings")]
     public static void ShowWindow()
     {
         GetWindow<ChangeSpritesSettings>("Change Sprites Settings");
     }
 
-    // 인스펙터 창에 표시될 변수들
     private float pixelsPerUnit = 64f;
     private FilterMode filterMode = FilterMode.Point;
     private int maxTextureSize = 2048;
@@ -20,7 +16,6 @@ public class ChangeSpritesSettings : EditorWindow
     private TextureImporterFormat androidFormat = TextureImporterFormat.Automatic;
     private TextureImporterFormat iosFormat = TextureImporterFormat.Automatic;
 
-    // 선택된 스프라이트들의 설정을 변경하는 함수
     private void OnGUI()
     {
         GUILayout.Label("Change Sprite Settings", EditorStyles.boldLabel);
@@ -40,6 +35,7 @@ public class ChangeSpritesSettings : EditorWindow
         }
     }
 
+
     private void ApplySettingsToSelectedSprites()
     {
         foreach (Object obj in Selection.objects)
@@ -52,31 +48,30 @@ public class ChangeSpritesSettings : EditorWindow
                 textureImporter.spritePixelsPerUnit = pixelsPerUnit;
                 textureImporter.filterMode = filterMode;
 
-                // Override for Windows
                 TextureImporterPlatformSettings windowsSettings = textureImporter.GetPlatformTextureSettings("Standalone");
-                windowsSettings.overridden = true;
+                windowsSettings.overridden = false;
                 windowsSettings.maxTextureSize = maxTextureSize;
                 windowsSettings.format = windowsFormat;
+                windowsSettings.compressionQuality = 0;
                 textureImporter.SetPlatformTextureSettings(windowsSettings);
 
-                // Override for Android
                 TextureImporterPlatformSettings androidSettings = textureImporter.GetPlatformTextureSettings("Android");
-                androidSettings.overridden = true;
+                androidSettings.overridden = false;
                 androidSettings.maxTextureSize = maxTextureSize;
                 androidSettings.format = androidFormat;
+                androidSettings.compressionQuality = 0;
                 textureImporter.SetPlatformTextureSettings(androidSettings);
 
-                // Override for iOS
                 TextureImporterPlatformSettings iosSettings = textureImporter.GetPlatformTextureSettings("iPhone");
-                iosSettings.overridden = true;
+                iosSettings.overridden = false;
                 iosSettings.maxTextureSize = maxTextureSize;
                 iosSettings.format = iosFormat;
+                iosSettings.compressionQuality = 0;
                 textureImporter.SetPlatformTextureSettings(iosSettings);
 
-                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+                textureImporter.SaveAndReimport();
             }
         }
-
         Debug.Log("Settings applied to selected sprites.");
     }
 }
