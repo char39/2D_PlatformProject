4124a88ee59e4f4e52f816ddddfdbf0e79760000
Your Name <seongsun8330@naver.com>
2024-08-17 00:02:12 +0900

    0817_움직임만 추가

--- Assets/02_Scripts/Player/Player.cs
+++ Assets/02_Scripts/Player/Player.cs
@@ -2,15 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace PlayerCtrl
 {
-    void Start()
+    public class Player : MonoBehaviour
     {
-        
-    }
+        [HideInInspector]
+        public Vector2 velocity;
 
-    void Update()
-    {
-        
+        internal PlayerInput playerInput;        
+        internal Transform tr;
+        internal SpriteRenderer sr;
+        internal Rigidbody2D rb;
+
+        private int horizontal;
+
+        void Start()
+        {
+            playerInput = GetComponent<PlayerInput>();
+            tr = GetComponent<Transform>();
+            sr = GetComponent<SpriteRenderer>();
+            rb = GetComponent<Rigidbody2D>();
+            
+        }
+
+        void FixedUpdate()
+        {
+            Move();
+        }
+
+        void Update()
+        {
+            GetValue();
+        }
+
+        private void GetValue()
+        {
+            velocity = rb.velocity;
+            horizontal = playerInput.Horizontal_1p;
+        }
+
+        private void Move()
+        {
+            if (horizontal != 0)
+                rb.velocity = new Vector2(horizontal * 5f, rb.velocity.y);
+        }
+
+
     }
 }
---
+++ Assets/Editor/PlayerMovementRigidbodyEditor.cs
@@ -0,0 +1,41 @@
+using UnityEditor;
+using UnityEngine;
+using PlayerCtrl;
+
+[CustomEditor(typeof(Player))]                          // Mario_Move 스크립트를 인스펙터 창에 표시
+public class PlayerMovementRigidbodyEditor : Editor
+{
+    private const float updateInterval = 0.1f;                                     // 갱신 주기 (초 단위)
+    private double lastUpdateTime;                                                  // 마지막 갱신 시간
+
+    void OnEnable()
+    {
+        lastUpdateTime = EditorApplication.timeSinceStartup;                        // 마지막 갱신 시간 초기화
+        EditorApplication.update += OnEditorUpdate;                                 // 에디터 갱신 이벤트 추가 (OnEditorUpdate 함수 호출)
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.update -= OnEditorUpdate;                                 // 에디터 갱신 이벤트 제거 (OnEditorUpdate 함수 호출 중지)
+    }
+
+    void OnEditorUpdate()
+    {
+        if (EditorApplication.timeSinceStartup - lastUpdateTime >= updateInterval)  // 갱신 주기마다
+        {
+            Repaint();                                                              // 인스펙터 창 갱신
+            lastUpdateTime = EditorApplication.timeSinceStartup;                    // 마지막 갱신 시간 갱신
+        }
+    }
+
+    public override void OnInspectorGUI()                                           // 인스펙터 창에 표시할 내용
+    {
+        DrawDefaultInspector();                                                     // 기본 인스펙터 창 표시
+
+        Player playerMovement = (Player)target;                             // Mario_Move 스크립트를 가져옴
+
+        GUILayout.Space(10);                                                        // 공백 추가
+        GUILayout.Label("Velocity", EditorStyles.boldLabel);                        // "Velocity" 레이블 표시
+        EditorGUILayout.Vector2Field("Current Velocity", playerMovement.velocity);  // 현재 속도 표시
+    }
+}
